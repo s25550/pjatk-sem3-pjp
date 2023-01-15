8bff493450c3e0e2937d092cf5569864cbffae5d
s25550 <101464088+s25550@users.noreply.github.com>
2023-01-15 20:47:20 +0100

    2.2

---
+++ cwicz2/ListOfIntegers.groovy
@@ -0,0 +1,29 @@
+package cwicz2
+
+import javax.swing.JOptionPane
+
+List getInts() {
+	def numbers = []
+	def err = false
+	while (true) {
+		def message = "Podaj liczbę"
+		if (err) {
+			err = false
+			message = "Błąd. $message"
+		}
+		def input = JOptionPane.showInputDialog(null, message, "")
+		if (input == null || input.equals("")) {
+			break
+		}
+		if (input.isInteger()) {
+			Integer integer = Integer.parseInt(input)
+			numbers.push(integer)
+		} else {
+			err = true
+		}
+	}
+
+	return numbers
+}
+
+print getInts()
